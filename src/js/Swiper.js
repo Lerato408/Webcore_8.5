f9b1c3931c155387d9bab0cb40778cae4e0fd07c
Lerato <lerato.lucete@gmail.com>
2026-01-14 02:14:20 +0900

    updated swiper2

--- src/js/Swiper.js
+++ src/js/Swiper.js
@@ -1,24 +1,34 @@
-var swiper = new Swiper(".brand-swiper", {
-        slidesPerView: 1.5,
-        spaceBetween: 30,
-        slidesPerGroup: 3,
-        loop: true,
-        loopFillGroupWithBlank: true,
-        pagination: {
-          el: ".swiper-pagination",
-          clickable: true,
-        },
-         
-      });
-        var swiper = new Swiper(".repair-swiper", {
-        slidesPerView: 1.3,
-        spaceBetween: 30,
-        slidesPerGroup: 3,
-        loop: true,
-        loopFillGroupWithBlank: true,
-        pagination: {
-          el: ".swiper-pagination",
-          clickable: true,
-        },
-      
-      });
+var swiper = new Swiper('.brand-swiper', {
+  slidesPerView: 1.5,
+  spaceBetween: 30,
+  slidesPerGroup: 3,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true
+  }
+})
+var swiper = new Swiper('.repair-swiper', {
+  slidesPerView: 1.3,
+  spaceBetween: 30,
+  slidesPerGroup: 3,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true
+  }
+})
+
+var swiper = new Swiper('.service-swiper', {
+  slidesPerView: 1.5,
+  spaceBetween: 30,
+  slidesPerGroup: 3,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true
+  }
+})
