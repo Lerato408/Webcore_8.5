ef18d4aa0aaf99cfc756894536299e4041d6ec9a
Lerato <lerato.lucete@gmail.com>
2026-01-15 02:44:31 +0900

    updated swiper3

--- src/js/Swiper.js
+++ src/js/Swiper.js
@@ -22,7 +22,7 @@ var swiper = new Swiper('.repair-swiper', {
 })
 
 var swiper = new Swiper('.service-swiper', {
-  slidesPerView: 1.5,
+  slidesPerView: 1.3,
   spaceBetween: 30,
   slidesPerGroup: 3,
   loop: true,
