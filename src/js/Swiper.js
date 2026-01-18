cb991dbbd5fef5233d2b9c0718947eed782ef46b
Lerato <lerato.lucete@gmail.com>
2026-01-19 01:12:10 +0900

    swiper update 5

--- src/js/Swiper.js
+++ src/js/Swiper.js
@@ -1,15 +1,63 @@
-var swiper = new Swiper('.brand-swiper', {
-  slidesPerView: 1.5,
-  spaceBetween: 30,
-  slidesPerGroup: 3,
-  loop: true,
-  loopFillGroupWithBlank: true,
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true
+           
+    let swiper = null;
+
+function enableSwiper() {
+    swiper = new Swiper('.brand-swiper', {
+    slidesPerView: 1.5,
+    spaceBetween: 30,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true
+    }
+  });
+}
+
+function disableSwiper() {
+  if (swiper !== null) {
+    swiper.destroy(true, true);
+    swiper = null;
   }
-})
-var swiper = new Swiper('.repair-swiper', {
+}
+
+function checkWidth() {
+  const width = window.innerWidth;
+
+  if (width < 768) {
+    if (!swiper) {
+      enableSwiper();
+    }
+  } 
+  else {
+    disableSwiper();
+  }
+}
+
+
+window.addEventListener('load', checkWidth);
+
+window.addEventListener('resize', checkWidth);
+
+
+const btn = document.querySelector('.container-toggle');
+const label = btn.querySelector('.label');
+const arrow = btn.querySelector('.content__arrow--img');
+const list = document.querySelector('.brand-swiper__wrapper');
+
+if (btn && list) {
+ btn.addEventListener('click', () => {
+   list.classList.toggle('expanded');          
+    if (list.classList.contains('expanded')) {
+                   label.textContent = "hide";
+                  arrow.classList.add('rotated');
+                } 
+                else {
+                 label.textContent = "show all";
+                 arrow.classList.remove('rotated');
+                }
+  });
+ }
+
+var repairSwiper = new Swiper('.repair-swiper', {
   slidesPerView: 1.3,
   spaceBetween: 30,
   slidesPerGroup: 3,
@@ -21,7 +69,7 @@ var swiper = new Swiper('.repair-swiper', {
   }
 })
 
-var swiper = new Swiper('.service-swiper', {
+var serviceSwiper = new Swiper('.service-swiper', {
   slidesPerView: 1.3,
   spaceBetween: 30,
   slidesPerGroup: 3,
