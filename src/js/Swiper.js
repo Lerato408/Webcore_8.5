78e39cc4105cf85ce0aefbd9e674acd3ab51fe90
Lerato <lerato.lucete@gmail.com>
2026-01-20 01:49:25 +0900

    swiper updates7

--- src/js/Swiper.js
+++ src/js/Swiper.js
@@ -1,82 +1,147 @@
-           
-    let swiper = null;
+let brandSwiper = null
 
-function enableSwiper() {
-    swiper = new Swiper('.brand-swiper', {
+function enableBrandSwiper() {
+  brandSwiper = new Swiper('.brand-swiper', {
     slidesPerView: 1.5,
     spaceBetween: 30,
     pagination: {
       el: '.swiper-pagination',
       clickable: true
     }
-  });
+  })
 }
 
-function disableSwiper() {
-  if (swiper !== null) {
-    swiper.destroy(true, true);
-    swiper = null;
+function disableBrandSwiper() {
+  if (brandSwiper !== null) {
+    brandSwiper.destroy(true, true)
+    brandSwiper = null
   }
 }
 
-function checkWidth() {
-  const width = window.innerWidth;
+function checkBrandWidth() {
+  const width = window.innerWidth
 
   if (width < 768) {
-    if (!swiper) {
-      enableSwiper();
+    if (!brandSwiper) {
+      enableBrandSwiper()
     }
-  } 
-  else {
-    disableSwiper();
+  } else {
+    disableBrandSwiper()
   }
 }
 
+window.addEventListener('load', checkBrandWidth)
 
-window.addEventListener('load', checkWidth);
+window.addEventListener('resize', checkBrandWidth)
 
-window.addEventListener('resize', checkWidth);
+const btnBrand = document.querySelector('.container-toggle')
+const labelBrand = btnBrand.querySelector('.label')
+const arrowBrand = btnBrand.querySelector('.content__arrow--img')
+const listBrand = document.querySelector('.brand-swiper__wrapper')
 
+if (btnBrand && listBrand) {
+  btnBrand.addEventListener('click', () => {
+    listBrand.classList.toggle('expanded')
+    if (listBrand.classList.contains('expanded')) {
+      labelBrand.textContent = 'hide'
+      arrowBrand.classList.add('rotated')
+    } else {
+      labelBrand.textContent = 'show all'
+      arrowBrand.classList.remove('rotated')
+    }
+  })
+}
 
-const btn = document.querySelector('.container-toggle');
-const label = btn.querySelector('.label');
-const arrow = btn.querySelector('.content__arrow--img');
-const list = document.querySelector('.brand-swiper__wrapper');
+let repairSwiper = null
 
-if (btn && list) {
- btn.addEventListener('click', () => {
-   list.classList.toggle('expanded');          
-    if (list.classList.contains('expanded')) {
-                   label.textContent = "hide";
-                  arrow.classList.add('rotated');
-                } 
-                else {
-                 label.textContent = "show all";
-                 arrow.classList.remove('rotated');
-                }
-  });
- }
+function enableRepairSwiper() {
+  repairSwiper = new Swiper('.repair-swiper', {
+    slidesPerView: 1.5,
+    spaceBetween: 30,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true
+    }
+  })
+}
 
-var repairSwiper = new Swiper('.repair-swiper', {
-  slidesPerView: 1.3,
-  spaceBetween: 30,
-  slidesPerGroup: 3,
-  loop: true,
-  loopFillGroupWithBlank: true,
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true
+function disableRepairSwiper() {
+  if (repairSwiper !== null) {
+    repairSwiper.destroy(true, true)
+    repairSwiper = null
   }
-})
+}
 
-var serviceSwiper = new Swiper('.service-swiper', {
-  slidesPerView: 1.3,
-  spaceBetween: 30,
-  slidesPerGroup: 3,
-  loop: true,
-  loopFillGroupWithBlank: true,
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true
+function checkRepairWidth() {
+  const width = window.innerWidth
+
+  if (width < 768) {
+    if (!repairSwiper) {
+      enableRepairSwiper()
+    }
+  } else {
+    disableRepairSwiper()
   }
-})
+}
+
+window.addEventListener('load', checkRepairWidth)
+
+window.addEventListener('resize', checkRepairWidth)
+
+const btnRepair = document.querySelector('.container-toggle')
+const labelRepair = btnRepair.querySelector('.label')
+const arrowRepair = btnRepair.querySelector('.content__arrow--img')
+const listRepair = document.querySelector('.repair-swiper__wrapper')
+
+if (btnRepair && listRepair) {
+  btnRepair.addEventListener('click', () => {
+    listRepair.classList.toggle('expanded')
+    if (listRepair.classList.contains('expanded')) {
+      labelRepair.textContent = 'hide'
+      arrowRepair.classList.add('rotated')
+    } else {
+      labelRepair.textContent = 'show all'
+      arrowRepair.classList.remove('rotated')
+    }
+  })
+}
+
+let serviceSwiper = null
+
+function enableServiceSwiper() {
+  serviceSwiper = new Swiper('.service-swiper', {
+    slidesPerView: 1.5,
+    spaceBetween: 30,
+  })
+}
+
+function disableServiceSwiper() {
+  if (serviceSwiper !== null) {
+    serviceSwiper.destroy(true, true)
+    serviceSwiper = null
+  }
+}
+
+function checkServiceWidth() {
+  const width = window.innerWidth
+
+  if (width < 768) {
+    if (!serviceSwiper) {
+      enableServiceSwiper()
+    }
+  } else {
+    disableServiceSwiper()
+  }
+}
+
+window.addEventListener('load', checkServiceWidth)
+
+window.addEventListener('resize', checkServiceWidth)
+
+const btnService = document.querySelector('.container-toggle')
+const labelService = btnService.querySelector('.label')
+const arrowService = btnService.querySelector('.content__arrow--img')
+const listService = document.querySelector('.service-swiper__wrapper')
+
+
+
